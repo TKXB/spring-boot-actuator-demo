73058f3e54b2c0c1078d7186dfbb43280992ce9d
wangzhongyu <wang3919379@163.com>
2018-08-09 19:04:08 +0800

    'demo'

---
+++ src/main/java/com/demo/actuator/conf/CustomContributor.java
@@ -0,0 +1,13 @@
+package com.demo.actuator.conf;
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomContributor implements InfoContributor {
+    @Override
+    public void contribute(Info.Builder builder) {
+        builder.withDetail("custom","this is my custom").build();
+    }
+}
---
+++ src/main/java/com/demo/actuator/controller/demo.java
@@ -0,0 +1,14 @@
+package com.demo.actuator.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class demo {
+    @GetMapping("/hello")
+    public @ResponseBody String hello(){
+        return "hello";
+    }
+}
